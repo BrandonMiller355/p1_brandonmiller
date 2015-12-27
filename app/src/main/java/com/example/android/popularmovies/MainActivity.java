ba0a65ca68a88ef381acfc89b57223fee1d33c88
Brandon Miller <Brandon.Miller@BlueSombrero.com>
2015-12-27 14:56:00 -0500

    Got image adapter working, pulling in one image

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -1,8 +1,14 @@
+// Credits: http://developer.android.com/guide/topics/ui/layout/gridview.html
+
 package com.example.android.popularmovies;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -11,13 +17,24 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.gridview_movies);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "" + i, Toast.LENGTH_SHORT).show();
+            }
+        });
+        /* TODO: Delete this
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new MoviesFragment())
                     .commit();
         }
 
-        Log.v("main", getString(R.string.my_api_key));
+        Log.v("main", getString(R.string.my_api_key));*/
 
     }
 
